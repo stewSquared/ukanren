71572704f855a06d6c0e7e21ef4d2bbbe1a3bd63
Stewart Stewart <stewinsalot@gmail.com>
2016-01-15 01:20:21 -0800

    walk and various renames

--- MicroKanren.scala
+++ MicroKanren.scala
@@ -11,19 +11,27 @@
 
 trait MicroKanren {
   case class $tream[T](ts: T*)
-  // A State is a pair of a substitution and nonNegative Integer
-  case class State(substitution: Map[LVar, Any], counter: Int)
+  type Term = Any
   case class LVar(index: Int)
-
+  type Substitution = Map[LVar, Term]
+  case class State(substitution: Substitution, counter: Int)
   type Goal = State => $tream[State]
 
   // Four primitive goal constructors
   def callFresh(f: LVar => Goal): Goal
-  def ===(u: Any, v: Any): Goal
+  def ===(u: Term, v: Term): Goal
   def disj(g1: Goal, g2: Goal): Goal
   def conj(g1: Goal, g2: Goal): Goal
 
   def emptyState = State(Map.empty, 0)
+
+  // Search for a variable's value in a substitution
+  def walk(u: Term, s: Substitution): Term = u match {
+    case v: LVar => s.get(v).fold(u)(walk(_, s))
+    case _ => u
+  }
+
+  def extS(x: LVar, v: Term, s: Substitution): Substitution = s + (x -> v)
 }
 
 object ukanren extends MicroKanren {
