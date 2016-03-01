02ba75729a723503885529e563eece3d330b41e6
Stewart Stewart <stewinsalot@gmail.com>
2016-02-29 22:05:20 -0800

    Simpler/clearer method names in UserInterface

--- MicroKanren.scala
+++ MicroKanren.scala
@@ -122,32 +122,25 @@ trait UserInterface extends Core {self: Reification =>
   }
 
   implicit class GoalOps(g: Goal) {
-    def |||(g2: Goal): Goal = disj(Zzz(g), Zzz(g2))
-    def &&&(g2: Goal): Goal = conj(Zzz(g), Zzz(g2))
+    def ||(g2: Goal): Goal = disj(Zzz(g), Zzz(g2))
+    def &&(g2: Goal): Goal = conj(Zzz(g), Zzz(g2))
   }
 
-  def disj_*(goals: ByName[Goal]*): Goal =
+  def any(goals: ByName[Goal]*): Goal =
     goals.headOption.fold(fail)(head =>
-      disj(Zzz(head()), disj_*(goals.tail: _*)))
+      disj(Zzz(head()), any(goals.tail: _*)))
 
-  def conj_*(goals: ByName[Goal]*): Goal =
+  def all(goals: ByName[Goal]*): Goal =
     goals.headOption.fold(succeed)(head =>
-      conj(Zzz(head()), conj_*(goals.tail: _*)))
+      conj(Zzz(head()), all(goals.tail: _*)))
 
-  def fresh(f: () => Goal): Goal = f()
-
-  def fresh(f: (LVar) => Goal): Goal =
-    callFresh(f)
-
-  def fresh(f: (LVar, LVar) => Goal): Goal =
-    callFresh(q =>
-      callFresh(r => f(q,r)))
-
-  // TODO: This is mechanical enough to be a macro
-  def fresh(f: (LVar, LVar, LVar) => Goal): Goal =
-    callFresh(q =>
-      callFresh(r =>
-        callFresh(s => f(q,r,s))))
+  def exists(f: () => Goal): Goal = f()
+  def exists(f: (LVar) => Goal): Goal = callFresh(f) 
+  def exists(f: (LVar, LVar) => Goal): Goal = callFresh(q => exists(f(q,_)))
+  def exists(f: (LVar, LVar, LVar) => Goal): Goal = callFresh(q => exists(f(q,_,_)))
+  def exists(f: (LVar, LVar, LVar, LVar) => Goal): Goal = callFresh(q => exists(f(q,_,_,_)))
+  def exists(f: (LVar, LVar, LVar, LVar, LVar) => Goal): Goal = callFresh(q => exists(f(q,_,_,_,_)))
+  def exists(f: (LVar, LVar, LVar, LVar, LVar, LVar) => Goal): Goal = callFresh(q => exists(f(q,_,_,_,_,_)))
 
   protected def reify(lvars: LVar*)(state: State): Seq[Term] = {
     def uniqueInOrder[T](items: Seq[T]): Seq[T] = items
@@ -178,65 +171,65 @@ trait UserInterface extends Core {self: Reification =>
 
     walkedValues.map(reindexVars)
   }
+}
 
+trait ListRelations extends UserInterface { self: Reification =>
   def conso(head: Term, tail: Term, out: Term): Goal = lcons(head, tail) === out
   
   def heado(head: Term, all: Term): Goal =
-    fresh(tail => (conso(head, tail, all)))
+    exists(tail => (conso(head, tail, all)))
 
   def tailo(tail: Term, all: Term): Goal =
-    fresh(head => (conso(head, tail, all)))
+    exists(head => (conso(head, tail, all)))
 
   def emptyo(l: Term): Goal = l === Nil
 
-  def listo(l: Term): Goal = disj_*(
+  def listo(l: Term): Goal = any(
     emptyo(l),
-    fresh((h, t) =>
-      conso(h, t, l) &&& listo(t)))
+    exists((h, t) =>
+      conso(h, t, l) && listo(t)))
 
-  def appendo(a: Term, b: Term, result: Term): Goal = disj_*(
-    (emptyo(a) &&& (result === b)),
-    fresh((h, t, tb) => conj_*(
+  def appendo(a: Term, b: Term, result: Term): Goal = any(
+    (emptyo(a) && (result === b)),
+    exists((h, t, tb) => all(
       conso(h, t, a),
-      appendo(t, b, tb),
-      conso(h, tb, result))))
+      conso(h, tb, result),
+      appendo(t, b, tb))))
 
   def membero(elem: Term, list: Term): Goal =
-    fresh((head, tail) => conj_*(
+    exists((head, tail) => all(
       conso(head, tail, list),
-      (elem === head) ||| membero(elem, tail)))
+      (elem === head) || membero(elem, tail)))
 
-  def rembero(elem: Term, all: Term, tail: Term): Goal = disj_*(
-    conso(elem, tail, all),
-    fresh((headAll, tailAll, rec) => conj_*(
-      conso(headAll, tailAll, all),
-      conso(headAll, rec, tail),
-      rembero(elem, tailAll, rec))))
+  def rembero(elem: Term, list: Term, rest: Term): Goal = any(
+    conso(elem, rest, list),
+    exists((h, t, recur) => all(
+      conso(h, t, list),
+      conso(h, recur, rest),
+      rembero(elem, t, recur))))
 
   // Can SO if lists are not ground and conj'd with something
   def permuto(listX: Term, listY: Term): Goal =
-    sameLengtho(listX, listY) &&& disj_*(
-      emptyo(listX) &&& emptyo(listY),
-      fresh((x, xs, ys) => conj_*(
+    sameLengtho(listX, listY) && any(
+      emptyo(listX) && emptyo(listY),
+      exists((x, xs, ys) => all(
         conso(x, xs, listX),
         rembero(x, listY, ys),
         permuto(xs, ys))))
 
-  def sameLengtho(xlist: Term, ylist: Term): Goal = disj_*(
-    emptyo(xlist) &&& emptyo(ylist),
-    fresh((xtail, ytail) => conj_*(
+  def sameLengtho(xlist: Term, ylist: Term): Goal = any(
+    emptyo(xlist) && emptyo(ylist),
+    exists((xtail, ytail) => all(
       tailo(xtail, xlist),
       tailo(ytail, ylist),
       sameLengtho(xtail, ytail))))
 
-  def samePoso(xlist: Term, ylist: Term, x: Term, y: Term): Goal = conj_*(
-    disj_*(
-      heado(x, xlist) &&& heado(y, ylist),
-      fresh((xtail, ytail) => conj_*(
-        tailo(xtail, xlist),
-        tailo(ytail, ylist),
-        samePoso(xtail, ytail, x, y))))
-  )
+  def samePoso(xlist: Term, ylist: Term, x: Term, y: Term): Goal = any(
+    heado(x, xlist) && heado(y, ylist),
+    exists((xtail, ytail) => all(
+      tailo(xtail, xlist),
+      tailo(ytail, ylist),
+      samePoso(xtail, ytail, x, y))))
 }
 
 trait Reification extends UserInterface
@@ -245,17 +238,17 @@ trait StringReification extends Reification {
   def reifyS(lvars: LVar*)(state: State): String =
     reify(lvars: _*)(state).mkString("(", ", ", ")")
 
-  def run_*(f: () => Goal): Stream[String] =
-    pull(fresh(f)(emptyState)).map(reifyS())
+  def run(f: () => Goal): Stream[String] =
+    pull(exists(f)(emptyState)).map(reifyS())
 
-  def run_*(f: (LVar) => Goal): Stream[String] =
-    pull(fresh(f)(emptyState)).map(reifyS(LVar(0)))
+  def run(f: (LVar) => Goal): Stream[String] =
+    pull(exists(f)(emptyState)).map(reifyS(LVar(0)))
 
-  def run_*(f: (LVar, LVar) => Goal): Stream[String] =
-    pull(fresh(f)(emptyState)).map(reifyS(LVar(0), LVar(1)))
+  def run(f: (LVar, LVar) => Goal): Stream[String] =
+    pull(exists(f)(emptyState)).map(reifyS(LVar(0), LVar(1)))
 
-  def run_*(f: (LVar, LVar, LVar) => Goal): Stream[String] =
-    pull(fresh(f)(emptyState)).map(reifyS(LVar(0), LVar(1), LVar(2)))
+  def run(f: (LVar, LVar, LVar) => Goal): Stream[String] =
+    pull(exists(f)(emptyState)).map(reifyS(LVar(0), LVar(1), LVar(2)))
 }
 
 trait ConcreteReification extends Reification {
@@ -279,15 +272,15 @@ trait ConcreteReification extends Reification {
     }
   }
 
-  def run_*[T](f: (LVar) => Goal): Stream[Either[LVar, T]] =
-    pull(fresh(f)(emptyState)).map(reifyC[T](LVar(0)))
+  def run[T](f: (LVar) => Goal): Stream[Either[LVar, T]] =
+    pull(exists(f)(emptyState)).map(reifyC[T](LVar(0)))
 
-  def run_*[F[_] <: Seq[_], T](f: (LVar) => Goal)(implicit d: DummyImplicit)
+  def run[F[_] <: Seq[_], T](f: (LVar) => Goal)(implicit d: DummyImplicit)
       : Stream[Either[LVar, F[Either[LVar, T]]]] =
-    pull(fresh(f)(emptyState)).map(reifyCNested[F, T](LVar(0)))
+    pull(exists(f)(emptyState)).map(reifyCNested[F, T](LVar(0)))
 }
 
-trait DynamicReification extends Reification {
+trait DynamicReification extends Reification with ListRelations {
   val Seq(_0, _1, _2, _3, _4, _5, _6, _7, _8, _9) = 0 to 9 map LVar
 
   def reifyD()(state: State): Unit = ()
@@ -296,26 +289,40 @@ trait DynamicReification extends Reification {
     reify(q)(state).head
 
   def reifyD(q: LVar, r: LVar)(state: State): (Term, Term) =
-    reify(q, r)(state) match {
-      case Seq(a, b) => (a, b)
-    }
+    reify(q, r)(state) match { case Seq(a, b) => (a, b) }
 
   def reifyD(q: LVar, r: LVar, s: LVar)(state: State): (Term, Term, Term) =
-    reify(q, r, s)(state) match {
-      case Seq(a, b, c) => (a, b, c)
-    }
+    reify(q, r, s)(state) match { case Seq(a, b, c) => (a, b, c) }
 
-  def run_*(f: () => Goal): Stream[Term] =
-    pull(fresh(f)(emptyState)).map(reifyD())
+  def reifyD(q: LVar, r: LVar, s: LVar, t: LVar)(state: State): (Term, Term, Term, Term) =
+    reify(q, r, s, t)(state) match { case Seq(a, b, c, d) => (a, b, c, d) }
 
-  def run_*(f: (LVar) => Goal): Stream[Term] =
-    pull(fresh(f)(emptyState)).map(reifyD(LVar(0)))
+  def reifyD(q: LVar, r: LVar, s: LVar, t: LVar, u: LVar)(state: State) =
+    reify(q, r, s, t, u)(state) match { case Seq(a, b, c, d, e) => (a, b, c, d, e) }
 
-  def run_*(f: (LVar, LVar) => Goal): Stream[Term] =
-    pull(fresh(f)(emptyState)).map(reifyD(LVar(0), LVar(1)))
+  def reifyD(q: LVar, r: LVar, s: LVar, t: LVar, u: LVar, v: LVar)(state: State) =
+    reify(q, r, s, t, u, v)(state) match { case Seq(a, b, c, d, e, f) => (a, b, c, d, e, f) }
 
-  def run_*(f: (LVar, LVar, LVar) => Goal): Stream[Term] =
-    pull(fresh(f)(emptyState)).map(reifyD(LVar(0), LVar(1), LVar(2)))
+  def run(f: () => Goal): Stream[Term] =
+    pull(exists(f)(emptyState)).map(reifyD())
+
+  def run(f: (LVar) => Goal): Stream[Term] =
+    pull(exists(f)(emptyState)).map(reifyD(_0))
+
+  def run(f: (LVar, LVar) => Goal): Stream[Term] =
+    pull(exists(f)(emptyState)).map(reifyD(_0, _1))
+
+  def run(f: (LVar, LVar, LVar) => Goal): Stream[Term] =
+    pull(exists(f)(emptyState)).map(reifyD(_0, _1, _2))
+
+  def run(f: (LVar, LVar, LVar, LVar) => Goal): Stream[Term] =
+    pull(exists(f)(emptyState)).map(reifyD(_0, _1, _2, _3))
+
+  def run(f: (LVar, LVar, LVar, LVar, LVar) => Goal): Stream[Term] =
+    pull(exists(f)(emptyState)).map(reifyD(_0, _1, _2, _3, _4))
+
+  def run(f: (LVar, LVar, LVar, LVar, LVar, LVar) => Goal): Stream[Term] =
+    pull(exists(f)(emptyState)).map(reifyD(_0, _1, _2, _3, _4, _5))
 }
 
 object ukanrenString extends Core with StringReification
--- src/test/scala/MicroKanren.scala
+++ src/test/scala/MicroKanren.scala
@@ -141,81 +141,81 @@ object MicroKanrenSuite extends TestSuite {
         State(Map(q -> 1, s -> 2, r -> 0), 3)
       ) == "(1, 0, 2)")
 
-      assert(run_*(q => succeed) == Stream("(_0)"))
-      assert(run_*((q, r) => succeed) == Stream("(_0, _1)"))
-      assert(run_*((q, r) => q === r) == Stream("(_0, _0)"))
-      assert(run_*((q, r) => r === q) == Stream("(_0, _0)"))
-      assert(run_*((q, r, s) => q === r) == Stream("(_0, _0, _1)"))
-      assert(run_*((q, r, s) => q === s) == Stream("(_0, _1, _0)"))
-      assert(run_*((q, r, s) => r === s) == Stream("(_0, _1, _1)"))
+      assert(run(q => succeed) == Stream("(_0)"))
+      assert(run((q, r) => succeed) == Stream("(_0, _1)"))
+      assert(run((q, r) => q === r) == Stream("(_0, _0)"))
+      assert(run((q, r) => r === q) == Stream("(_0, _0)"))
+      assert(run((q, r, s) => q === r) == Stream("(_0, _0, _1)"))
+      assert(run((q, r, s) => q === s) == Stream("(_0, _1, _0)"))
+      assert(run((q, r, s) => r === s) == Stream("(_0, _1, _1)"))
 
       assert(Stream("(List(_0, _1))") ==
-        run_*(q => fresh((x, y) => (q === List(x, y)))))
+        run(q => exists((x, y) => (q === List(x, y)))))
 
       assert(Stream("(_0, List(_1, _2), _3)") ==
-        run_*((l, q, r) => fresh((w, x, y) => (q === List(x, y)))))
+        run((l, q, r) => exists((w, x, y) => (q === List(x, y)))))
 
       assert(Stream("(List(0), 0)") ==
-        run_*((q, r) => (q === List(r)) &&& (q === List(0))))
+        run((q, r) => (q === List(r)) && (q === List(0))))
 
       assert(Stream("(List(_0, _1, _2), _1)") ==
-        run_*((q, r) => fresh((x, y, z) => q === List(z, r, x))))
+        run((q, r) => exists((x, y, z) => q === List(z, r, x))))
 
       assert(Stream("(List(_0, _1), _2)") ==
-        run_*((q, r) => fresh((x, y, z) => q === List(y, z))))
+        run((q, r) => exists((x, y, z) => q === List(y, z))))
 
       assert(Stream("(_0, List(_1, _2))") ==
-        run_*((q, r) => fresh((x, y, z) => r === List(y, z))))
+        run((q, r) => exists((x, y, z) => r === List(y, z))))
     }
 
     "concrete reification"-{
       import ukanrenConcrete._
 
-      assert(run_*[Int](q => q === 4) ==
+      assert(run[Int](q => q === 4) ==
         Stream[Either[LVar, Int]](Right(4)))
 
-      assert(run_*[Int](q => succeed) ==
+      assert(run[Int](q => succeed) ==
         Stream[Either[LVar, Int]](Left(LVar(0))))
 
-      assert(run_*[List[Int]](q => q === List(4)) ==
+      assert(run[List[Int]](q => q === List(4)) ==
         Stream[Either[LVar, List[Int]]](Right(List(4))))
 
-      assert(run_*[List, Int](q => q === List(4)) ==
+      assert(run[List, Int](q => q === List(4)) ==
         Stream[Either[LVar, List[Either[LVar, Int]]]](Right(List(Right(4)))))
 
-      assert(run_*[List, Int](q => fresh(r => q === List(1, r, 3))) ==
+      assert(run[List, Int](q => exists(r => q === List(1, r, 3))) ==
         Stream[Either[LVar, List[Either[LVar, Int]]]](
           Right(List(Right(1), Left(LVar(0)), Right(3)))))
     }
 
     import ukanren._
 
-    "variadic conj/disj"-{
-      assert(run_*(() => disj_*(fail, fail, fail)).isEmpty)
-      assert(run_*(() => conj_*(succeed, succeed)).nonEmpty)
+    "variadic any/all"-{
+      assert(run(() => any(fail, fail, fail)).isEmpty)
+      assert(run(() => all(succeed, succeed)).nonEmpty)
 
-      val multiconj = fresh((q,r,s) =>
-        conj_*((q === 3), (r === 4), (s === q)))
+      val multiconj = exists((q,r,s) =>
+        all((q === 3), (r === 4), (s === q)))
 
       assert(pull(multiconj(emptyState)).toList ==
         List(State(Map(LVar(0) -> 3, LVar(1) -> 4, LVar(2) -> 3),3)))
 
-      def threes(x: LVar): Goal = disj_*(threes(x), (x === 3), threes(x))
-      assert(run_*(threes _).take(3) == Stream(3, 3, 3))
+      def threes(x: LVar): Goal = any(threes(x), (x === 3), threes(x))
+      assert(run(threes _).take(3) == Stream(3, 3, 3))
 
-      def notThrees(x: LVar): Goal = conj_*((x === 3), (x === 4), notThrees(x))
-      assert(run_*(notThrees _).isEmpty)
+      def notThrees(x: LVar): Goal = all((x === 3), (x === 4), notThrees(x))
+      assert(run(notThrees _).isEmpty)
     }
 
-    "variaic fresh"-{
-      val g2 = fresh((q,r) => conj_*(
+    "variaic exist"-{
+      val g2 = exists((q,r) => all(
         (3 === q), (r === 4)))
 
       assert(
         pull(g2(emptyState)).toList == List(
           State(Map(LVar(0) -> 3, LVar(1) -> 4),2)))
 
-      val g3 = fresh((q,r,s) => conj_*(
+      val g3 = exists((q,r,s) => all(
         (0 === r),
         (q === s),
         (s === r)))
@@ -228,55 +228,55 @@ object MicroKanrenSuite extends TestSuite {
       val (x, y) = (LVar(0), LVar(1))
 
       assert(State(Map(x -> 1, y -> 3), 2) ==
-        pull(fresh((x, y) => (List(x, 2, 3) === List(1, 2, y)))(emptyState)).head
+        pull(exists((x, y) => (List(x, 2, 3) === List(1, 2, y)))(emptyState)).head
       )
 
       assert(State(Map(x -> (List(1, y, 3))), 2) ==
-        pull(fresh((x, y) => (x === List(1, y, 3)))(emptyState)).head
+        pull(exists((x, y) => (x === List(1, y, 3)))(emptyState)).head
       )
     }
 
-    "run_* interface"-{
-      assert(run_*(() => fail) == Stream())
-      assert(run_*(() => succeed) == Stream((): Unit))
-      assert(run_*((q, r, s) => (r === s)) == Stream((_0, _1, _1)))
+    "run interface"-{
+      assert(run(() => fail) == Stream())
+      assert(run(() => succeed) == Stream((): Unit))
+      assert(run((q, r, s) => (r === s)) == Stream((_0, _1, _1)))
     }
 
     "syntax examples"-{
       def teacup(x: LVar) =
-        ('tea === x) ||| ('cup === x)
+        ('tea === x) || ('cup === x)
 
-      assert(run_*(x => teacup(x)) == Stream(('tea), ('cup)))
+      assert(run(x => teacup(x)) == Stream(('tea), ('cup)))
     }
 
     "lcons"-{
       assert(Stream((_0)) ==
-        run_*(q => conso(0, List(1, 2, 3), List(0, 1, 2, 3))))
+        run(q => conso(0, List(1, 2, 3), List(0, 1, 2, 3))))
 
       assert(Stream((0, 1, 2)) ==
-        run_*((q, r, s) => conso(q, List(r, 2, 3), List(0, 1, s, 3))))
+        run((q, r, s) => conso(q, List(r, 2, 3), List(0, 1, s, 3))))
 
       assert(Stream(List(1, 2, 3)) ==
-        run_*(q => conso(0, q, List(0, 1, 2, 3))))
+        run(q => conso(0, q, List(0, 1, 2, 3))))
 
       assert(Stream(List(0, 1, 2, 3)) ==
-        run_*(q => conso(0, List(1, 2, 3), q)))
+        run(q => conso(0, List(1, 2, 3), q)))
 
-      assert(run_*((q, r) => conso(0, q, r)) == Stream((_0, 0::_0)))
-      assert(run_*((q, r) => conso(q, List(1), r)) == Stream((_0, List(_0, 1))))
-      assert(run_*((q, r) => conso(q, r, List(1))) == Stream((1, List())))
-      assert(run_*(conso _) == Stream((_0, _1, _0::_1)))
+      assert(run((q, r) => conso(0, q, r)) == Stream((_0, 0::_0)))
+      assert(run((q, r) => conso(q, List(1), r)) == Stream((_0, List(_0, 1))))
+      assert(run((q, r) => conso(q, r, List(1))) == Stream((1, List())))
+      assert(run(conso _) == Stream((_0, _1, _0::_1)))
 
       assert(Stream((_0::_1::_2)) ==
-        run_*(out =>
-          fresh((q, r, s) =>
-            fresh(l =>
-              conso(r, s, l) &&& conso(q, l, out)))))
+        run(out =>
+          exists((q, r, s) =>
+            exists(l =>
+              conso(r, s, l) && conso(q, l, out)))))
 
       assert(Stream((_0, _1, _0::_1)) ==
-        run_*((a, d, l) =>
-          fresh((a2, d2) =>
-            conso(a, d, l) &&& conso(a2, d2, l))))
+        run((a, d, l) =>
+          exists((a2, d2) =>
+            conso(a, d, l) && conso(a2, d2, l))))
     }
       
     "other list operations"-{
@@ -286,35 +286,35 @@ object MicroKanrenSuite extends TestSuite {
         (List("first", _0, _1, "last")),
         (List("first", _0, _1, _2, "last"))
       ) ==
-        run_*(result  =>
-          fresh((listA, listB, tailA) => conj_*(
+        run(result  =>
+          exists((listA, listB, tailA) => all(
             conso("first", tailA, listA),
             listB === List("last"),
             appendo(listA, listB, result)))
         ).take(4))
 
-      assert(run_*(q => heado(q, 1 to 10)) == Stream(1))
+      assert(run(q => heado(q, 1 to 10)) == Stream(1))
 
-      assert(run_*(q => rembero(q, List(1, 2), List(1))) == Stream(2))
-      assert(run_*(q => rembero(q, List(1, 2), List(2))) == Stream(1))
-      assert(run_*(q => rembero(2, q, List(1))) == Stream(List(2, 1), List(1, 2)))
-      assert(run_*(q => rembero(1, List(1, 2), q)) == Stream(List(2)))
-      assert(run_*(q => rembero(2, List(1, 2), q)) == Stream(List(1)))
+      assert(run(q => rembero(q, List(1, 2), List(1))) == Stream(2))
+      assert(run(q => rembero(q, List(1, 2), List(2))) == Stream(1))
+      assert(run(q => rembero(2, q, List(1))) == Stream(List(2, 1), List(1, 2)))
+      assert(run(q => rembero(1, List(1, 2), q)) == Stream(List(2)))
+      assert(run(q => rembero(2, List(1, 2), q)) == Stream(List(1)))
 
-      assert(run_*(q => rembero(3, q, List(1, 2))) ==
+      assert(run(q => rembero(3, q, List(1, 2))) ==
         Stream(List(3, 1, 2), List(1, 3, 2), List(1, 2, 3)))
 
-      assert(run_*(q => membero(3, q)).take(3)
+      assert(run(q => membero(3, q)).take(3)
         == Stream((3::_0), (_0::3::_1), (_0::_1::3::_2)))
-      assert(run_*(q => membero(q, List(1, 2, 3))) == Stream(1, 2, 3))
-      assert(run_*(q => membero(2, List(1, q, 3))) == Stream(2))
-      assert(run_*(q => membero(1, List(1, q, 3))).toSet == Set(1, _0))
+      assert(run(q => membero(q, List(1, 2, 3))) == Stream(1, 2, 3))
+      assert(run(q => membero(2, List(1, q, 3))) == Stream(2))
+      assert(run(q => membero(1, List(1, q, 3))).toSet == Set(1, _0))
 
-      assert(run_*(q => permuto(q, 1 to 4)).toSet
+      assert(run(q => permuto(q, 1 to 4)).toSet
         == (1 to 4).permutations.toSet)
 
-      assert(run_*(q => permuto(q, List(1))) == Stream(List(1)))
-      assert(run_*(q => permuto(List(1), q)) == Stream(List(1)))
+      assert(run(q => permuto(q, List(1))) == Stream(List(1)))
+      assert(run(q => permuto(List(1), q)) == Stream(List(1)))
     }
   }
 }
