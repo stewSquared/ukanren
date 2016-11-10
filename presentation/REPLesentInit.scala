70e57e5a71830ad7f1b5b52f1b48dcfba1870c7f
Stewart Stewart <stewinsalot@gmail.com>
2016-11-10 15:07:02 -0800

    simplify presentation initialization

---
+++ presentation/REPLesentInit.scala
@@ -0,0 +1,3 @@
+val replesent = REPLesent(slideCounter = true, slideTotal = true, intp=$intp)
+import replesent._
+import ukanren._
--- presentation/start.sh
+++ presentation/start.sh
@@ -1,1 +1,1 @@
-scala -Dscala.color -language:_ -nowarn -i REPLesent.scala -cp $HOME/.ivy2/local/com.stewsquared/ukanren_2.11/0.1-SNAPSHOT/jars/ukanren_2.11.jar
+scala -Dscala.color -language:_ -nowarn -i REPLesent.scala -i REPLesentInit.scala -cp $HOME/.ivy2/local/com.stewsquared/ukanren_2.11/0.1-SNAPSHOT/jars/ukanren_2.11.jar
